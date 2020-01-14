21df311351367080178f67cc4b7f77b4e703213a
qingwei <shuaiwei0323@163.com>
2020-01-14 09:06:31 +0800

    主页基础完成

---
+++ src/views/home/components/article-list.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class='scroll-wrapper'>
+      <van-list></van-list>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'aritcle-list',
+  data () {
+    return {
+      upLoading: false, // 是否开启上拉加载
+      finished: false // 是否完成全部加载
+    }
+  }
+}
+</script>
+
+<style lang="less">
+</style>
